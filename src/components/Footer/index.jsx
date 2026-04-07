3d9bdcbebf25f7886290aa953c0d79555144961d
Liting <janetli82@gmail.com>
2026-04-07 10:36:08 +0200

    adding some contents in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,18 @@
 import styles from "./footer.module.css"
+import { Link } from "react-router-dom"
 
 const Footer = () => {
   return(
     <footer className={styles.footer}>
-      <p>&copy; KungFu Stars 2026</p>
+      <p>&copy; 2026 KungFu Stars </p>
+      <div className={styles.links}>
+        <Link to="/" className={styles.item}>Home</Link>
+        <Link to="/stars" className={styles.item}>Stars</Link>
+        <Link to="/movies" className={styles.item}>Movies</Link>
+      </div>
+      <p className={styles.credit}>
+        Data from Wikipedia and OMDb API
+      </p>
     </footer>
   )
 }
